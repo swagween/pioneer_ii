b8f7e27ff0dc19fd9785d4bf4876494bff91b89f
Alex Frasca <alexfrasca3@gmail.com>
2024-04-02 21:48:59 -0400

    add demo

---
+++ demo/src/entities/player/Player.hpp
@@ -0,0 +1,246 @@
+//
+//  Player.hpp
+//  for_loop
+//
+//  Created by Alex Frasca on 10/12/18.
+//  Copyright © 2018 Western Forest Studios. All rights reserved.
+//
+#pragma once
+
+#include "../../utils/Collider.hpp"
+#include "../../components/PhysicsComponent.hpp"
+#include "../../components/BehaviorComponent.hpp"
+#include "../../weapon/Arsenal.hpp"
+#include "../../utils/BitFlags.hpp"
+#include <array>
+#include <memory>
+
+const float PLAYER_WIDTH = 24.0f;
+const float PLAYER_HEIGHT = 24.0f;
+const float PLAYER_START_X = 100.0f;
+const float PLAYER_START_Y = 100.0f;
+const float JUMPBOX_HEIGHT = 8.0f;
+const float DETECTOR_WIDTH = 8.0f;
+const float DETECTOR_HEIGHT = 22.0f;
+const float WALL_SLIDE_DETECTOR_OFFSET = 20.0f;
+const float DETECTOR_BUFFER = (PLAYER_HEIGHT - DETECTOR_HEIGHT) / 2;
+const int JUMP_BUFFER_TIME = 12;
+const int INVINCIBILITY_TIME = 200;
+const int ANCHOR_BUFFER = 50;
+const int num_sprites{220};
+
+struct PlayerStats {
+    
+    int health{};
+    int max_health{};
+    int orbs{};
+    int max_orbs{};
+    
+};
+
+struct PlayerInventoryStats {
+    
+    uint32_t gem_spinel{};
+    uint32_t gem_topaz{};
+    uint32_t gem_tourmaline{};
+    uint32_t gem_peridot{};
+    
+    uint32_t flower_lavender{};
+    uint32_t flower_daffodil{};
+    uint32_t flower_hibiscus{};
+    uint32_t flower_orchid{};
+    
+};
+
+
+struct PhysicsStats {
+
+    float PLAYER_MAX_XVEL = 2.380f;
+    float PLAYER_MAX_YVEL = 0.226f;
+
+    float AIR_MULTIPLIER = 2.912f;
+
+    float PLAYER_GRAV = 0.002f;
+
+    float TERMINAL_VELOCITY = 0.8f;
+
+    float PLAYER_GROUND_FRIC = 0.963f;
+    float PLAYER_HORIZ_AIR_FRIC = 0.987f;
+    float PLAYER_VERT_AIR_FRIC = 0.956f;
+
+    float X_ACC = 0.056f;
+    float X_ACC_AIR = 0.056f;
+
+    //float JUMP_MAX = 0.304f; //3 blocks
+    float JUMP_MAX = 0.392f; //4 blocks
+    
+    float WALL_SLIDE_THRESHOLD = -1.0f;
+    float WALL_SLIDE_SPEED = 1.31f;
+    float PLAYER_MASS = 1.0f;
+
+    float JUMP_RELEASE_MULTIPLIER = 0.65f;
+
+    float HURT_ACC = 0.15f;
+    
+};
+
+struct Counters {
+    int invincibility{};
+};
+
+enum class Soundboard {
+    jump,
+    step,
+    land,
+    weapon_swap,
+    hurt,
+};
+
+enum class Jump {
+    hold, //true if jump is pressed and permanently false once released, until player touches the ground again
+    trigger, //true for one frame if jump is pressed and the player is grounded
+    can_jump, //true if the player is grounded
+    just_jumped, //used for updating animation
+    is_pressed, //true if the jump button is pressed, false if not. independent of player's state.
+    is_released, //true if jump released midair, reset upon landing
+    jumping, //true if jumpsquat is over, falce once player lands
+};
+
+enum class Movement {
+
+	move_left,
+	move_right,
+	look_up,
+	look_down,
+	left_released,
+	right_released,
+
+	stopping,
+	just_stopped,
+	suspended_trigger,
+	fall_trigger,
+	landed_trigger,
+	entered_freefall,
+	freefalling,
+    autonomous_walk,
+
+	is_wall_sliding,
+	wall_slide_trigger,
+	release_wallslide,
+};
+
+enum class Input {
+    restricted,
+    no_anim,
+    exit_request,
+    inspecting,
+    inspecting_trigger
+};
+
+enum class State {
+    alive
+};
+
+struct PlayerFlags {
+    util::BitFlags<Soundboard> sounds{};
+    util::BitFlags<Jump> jump{};
+    util::BitFlags<Movement> movement{};
+    util::BitFlags<Input> input{};
+    util::BitFlags<State> state{};
+};
+
+class Player {
+public:
+
+    using Clock = std::chrono::steady_clock;
+    using Time = std::chrono::duration<float>;
+    Player();
+    
+    //member functions
+    void handle_events(sf::Event& event);
+    void update(Time dt);
+    void render(sf::RenderWindow& win, sf::Vector2<float>& campos);
+    void assign_texture(sf::Texture& tex);
+    void update_animation();
+    void update_sprite();
+    void flash_sprite();
+    void calculate_sprite_offset();
+    
+    void update_behavior();
+    void set_position(sf::Vector2<float> new_pos);
+    void update_direction();
+    void update_weapon();
+    void walk();
+    void autonomous_walk();
+
+    void restrict_inputs();
+    void unrestrict_inputs();
+    void restrict_animation();
+    void no_move();
+
+    bool grounded() const;
+    bool moving() const;
+    bool moving_at_all() const;
+    
+    //firing
+    sf::Vector2<float> get_fire_point();
+
+    //level events
+    void make_invincible();
+    void update_invincibility();
+    bool is_invincible() const;
+    void kill();
+    void start_over();
+
+    //map helpers
+    behavior::DIR_LR entered_from();
+    
+    //sound
+    void play_sounds();
+    
+    //for debug mode
+    std::string print_direction(bool lr);
+
+    shape::Collider collider{ {PLAYER_WIDTH, PLAYER_HEIGHT}, {PLAYER_START_X, PLAYER_START_Y} };
+    components::PlayerBehaviorComponent behavior{};
+    behavior::DIR last_dir{};
+    arms::Arsenal loadout{};
+    std::vector<arms::WEAPON_TYPE> weapons_hotbar{};
+    int current_weapon{};
+    
+    sf::Vector2<float> apparent_position{};
+    sf::Vector2<float> anchor_point{};
+    sf::Vector2<float> hand_position{};
+    sf::Vector2<float> sprite_offset{};
+    
+    PlayerStats player_stats{3, 3, 0, 100};
+    PlayerInventoryStats player_inv_stats{0, 0, 0, 0, 0, 0, 0, 0};
+    PhysicsStats stats{};
+    PlayerFlags flags{};
+    
+    Counters counters{};
+
+    //fixed animation time step variables
+    Time dt{ 0.001f };
+    Clock::time_point current_time = Clock::now();
+    Time accumulator{ 0.0f };
+
+    //sprites
+    sf::Sprite sprite{};
+    
+    bool grav = true;
+
+    
+    int jump_request{};
+
+    bool just_hurt{};
+    
+    bool weapon_fired{};
+    bool start_cooldown{};
+
+    bool sprite_flip{};
+
+    int wall_slide_ctr{0};
+    
+};
+ /* Player_hpp */
---
+++ demo/src/particle/Emitter.hpp
@@ -0,0 +1,140 @@
+//
+//  Emitter.hpp
+//  fornani
+//
+//  Created by Alex Frasca on 01/05/2023.
+//
+#pragma once
+
+#include "ObjectPool.hpp"
+#include "Particle.hpp"
+#include "../components/PhysicsComponent.hpp"
+#include <vector>
+
+namespace vfx {
+
+const uint32_t default_size = 64;
+using Time = std::chrono::duration<float>;
+
+enum class EMMITER_TYPE {
+    ET_INFINITE,
+    ET_BURST,
+    ET_FADEOUT,
+    ET_FADEIN
+};
+
+struct ElementBehavior {
+    int rate{}; //expulsion rate
+    int rate_variance{}; //variance in expulsion rate
+    float expulsion_force{};
+    float expulsion_variance{};
+    float cone{}; //angle in radians of element dispersal
+    float grav{};
+    float grav_variance{};
+    float x_friction{};
+    float y_friction{};
+};
+
+struct EmitterStats {
+    int lifespan{};
+    int lifespan_variance{};
+    int particle_lifespan{};
+    int particle_lifespan_variance{};
+    float part_size = 3.0f;
+};
+
+class Emitter {
+    
+public:
+    
+    Emitter() = default;
+    Emitter(ElementBehavior b, EmitterStats s, sf::Color c = sf::Color{255, 255, 255}) : behavior(b), stats(s), color(c) {
+        util::Random r{};
+        int var = r.random_range(-stats.lifespan_variance, stats.lifespan_variance);
+        stats.lifespan += var;
+    }
+    ~Emitter() { particles.clear(); }
+    
+    void update() { //this will tick every element and the generator itself
+        physics.update();
+        if(stats.lifespan > 0) { //make a particle at a certain rate
+            for (int i = 0; i < behavior.rate; ++i) {
+                particles.push_back(Particle(physics, behavior.expulsion_force, behavior.expulsion_variance, behavior.cone, { behavior.x_friction, behavior.y_friction }, stats.part_size));
+                particles.back().physics.dir = physics.dir;
+                util::Random r{};
+                int var = r.random_range(-stats.particle_lifespan_variance, stats.particle_lifespan_variance);
+                particles.back().lifespan = stats.particle_lifespan + var;
+            }
+        }
+
+        for (auto& particle : particles) {
+            particle.update(behavior.expulsion_force, behavior.grav, behavior.grav_variance);
+        }
+
+        std::erase_if(particles, [](auto const& p) { return p.lifespan < 0; });
+
+        --stats.lifespan;
+    }
+
+    bool empty() {
+        return particles.empty();
+    }
+    
+    void set_position(float x, float y) {
+        physics.position.x = x;
+        physics.position.y = y;
+        for(auto& particle : particles) {
+            particle.physics.position = {x, y};
+        }
+    }
+    void set_velocity(float x, float y) {
+        physics.velocity.x = x;
+        physics.velocity.y = y;
+    }
+    
+    void apply_force(sf::Vector2<float> force) {
+        physics.apply_force(force);
+        for(auto& particle : particles) {
+            particle.physics.apply_force(force);
+        }
+    }
+    void apply_force_at_angle(float force, float angle) {
+        physics.apply_force_at_angle(force, angle);
+        for(auto& particle : particles) {
+            particle.physics.apply_force_at_angle(force, angle);
+        }
+    }
+    
+    components::PhysicsComponent& get_physics() { return physics; }
+    ElementBehavior& get_behavior() { return behavior; }
+    
+    void set_rate(float r) { behavior.rate = r; }
+    void set_expulsion_force(float f) { behavior.expulsion_force = f; }
+    void set_friction(float f) { physics.friction = {f, f}; }
+    void set_lifespan(int l) { stats.lifespan = l; }
+    void set_direction(components::DIRECTION d) { physics.dir = d; }
+    
+    std::vector<Particle>& const get_particles() {
+        return particles;
+    }
+    
+    int get_lifespan() { return stats.lifespan; }
+    int num_particles () { return (int)particles.size(); }
+    
+    sf::Color color{};
+    
+    std::vector<Particle> particles{};
+    ElementBehavior behavior{};
+    EmitterStats stats{};
+    components::PhysicsComponent physics{};
+    
+    EMMITER_TYPE type = EMMITER_TYPE::ET_BURST;
+    uint32_t max_size = default_size;
+    
+};
+
+} // end namespace
+
+
+
+/* Emitter_hpp */
